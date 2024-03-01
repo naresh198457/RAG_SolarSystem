c428e042b767d35b0146a3412a6918948860e568
naresh198457 <naresh.sampara@gmail.com>
2024-03-01 12:25:47 +0000

    add bits to the code

--- RAG_Model.py
+++ RAG_Model.py
@@ -16,9 +16,8 @@ os.environ['PINECONE_ENVIRONMENT']='gcp-starter'
 # initialize Connection to pinecone
 api_key=os.getenv('PINECONE_API_KEY')
 env=os.getenv('PINECONE_ENVIRONMENT')
-
-
 pc=Pinecone(api_key=os.environ.get('PINECONE_API_KEY'))
+
 # connect to existing Index:
 index=pc.Index('my-solar-system')
 index.describe_index_stats()
@@ -45,7 +44,7 @@ def generate_augmented_query(query,embed_model='text-embedding-ada-002',k=5):
 # sending system prompt and user prompt to the LLM. 
 def ask_gpt(system_prompt, user_prompt,model='gpt-3.5-turbo'):
 
-  temperature_=0.7
+  temperature_=0.7 # Tunning parameter
   completion = client.chat.completions.create(model=model,
                                               temperature=temperature_,
                                               messages=[{'role':'system',
@@ -61,7 +60,7 @@ def ask_gpt(system_prompt, user_prompt,model='gpt-3.5-turbo'):
 def rag_response(query):
 
     embed_model='text-embedding-ada-002'
-
+    # System prompt is 
     primer=f"""
     You are a expert in solar system. A highly intelligent system that answer
     user question based on information provided by the user above each question.
@@ -69,7 +68,7 @@ def rag_response(query):
     "I don't know".
     """
 
-    llm_model='gpt-3.5-turbo'
+    llm_model='gpt-3.5-turbo' # LLM Model
     user_prompt, ref=generate_augmented_query(query,embed_model=embed_model)
     system_prompt=primer
     return ask_gpt(system_prompt,user_prompt,model=llm_model), user_prompt, ref
